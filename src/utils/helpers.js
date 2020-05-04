b21b807d5d1e82e598709ba85d5f8795d56a063e
Shawn <pan.conan@gmail.com>
2020-05-04 17:24:09 +0800

    update URL of backend

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import Swal from 'sweetalert2'
-const baseURL = 'http://localhost:3000/api'
+const baseURL = 'https://masungo-backend.herokuapp.com/api'
 export const apiHelper = axios.create({ baseURL })
 
 export const Toast = Swal.mixin({
