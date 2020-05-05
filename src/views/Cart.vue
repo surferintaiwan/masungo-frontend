0ce4a0dc55cbd622207e16cf8ada6ca707da4ee4
Shawn <pan.conan@gmail.com>
2020-05-05 17:28:11 +0800

    update Cart page to correct totalPrice when user delete item in the cart

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="container py-5">
     <h1>購物車</h1>
-    <CartTable
-      v-bind:cart="cart"
-      v-bind:totalPrice="totalPrice"
-      v-on:after-delete-cart-item="afterDeleteCartItem"
-    />
-    <div class="text-right">
-      <router-link to="/checkout" class="btn btn-info mr-2 text-right">前往結帳</router-link>
-    </div>
+    <template v-if="this.cart.CartItems.length !== 0">
+      <CartTable
+        v-bind:cart="cart"
+        v-bind:totalPrice="totalPrice"
+        v-on:after-delete-cart-item="afterDeleteCartItem"
+      />
+
+      <div class="text-right">
+        <router-link to="/checkout" class="btn btn-info mr-2 text-right">前往結帳</router-link>
+      </div>
+    </template>
+    <template v-else>
+      <h2>---購物車內尚無商品---</h2>
+    </template>
   </div>
 </template>
 
@@ -35,7 +41,6 @@ export default {
         // 取出存在瀏覽器的cartId送去給API
         const cartId = localStorage.getItem("cartId");
         let response = await cartsAPI.getCartItems({ cartId });
-        console.log(response);
         const { data, statusText } = response;
         if (statusText !== "OK") {
           throw new Error(statusText);
@@ -60,10 +65,19 @@ export default {
         if (statusText !== "OK" || data.status !== "success") {
           throw new Error(statusText);
         }
-        // 確定得到API回覆刪除成功了，記得接著要把這個元件內的資料做刪除，否則要到使用者刷新頁面才會看到刪除後的結果
+        // 確定得到API回覆刪除成功了，記得接著要把這個元件內的資料做刪除，否則變成等到使用者刷新頁面才會看到刪除後的結果
         this.cart.CartItems = this.cart.CartItems.filter(cartItem => {
           return cartItem.ProductId !== productId;
         });
+        // 再來是totalPrice也要商品金額重新做加總，但要先判斷購物車裡面還有沒有商品
+        if (this.cart.CartItems.length !== 0) {
+          this.cart.CartItems.forEach(cartItem => {
+            this.totalPrice = 0;
+            this.totalPrice = cartItem.Product.sellingPrice * cartItem.quantity;
+          });
+        } else {
+          this.totalPrice = 0;
+        }
       } catch (error) {
         console.log("error", error);
         Toast.fire({
